8bd3680f13e33f0a66f62377dc433de9ae0d8fda
Evgeny Cherkashin <eugene@irnok.net>
2017-07-07 02:25:37 +0800

    Added alignment operation stub. Icon wanted!

---
+++ src/main/java/com/rapidminer/ngs/operator/MothurAlignOperator.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package com.rapidminer.ngs.operator;
+
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.operator.ports.InputPort;
+import com.rapidminer.operator.ports.OutputPort;
+
+/**
+ * @author eugeneai
+ *
+ */
+public class MothurAlignOperator extends MothurOperator {
+
+	/**
+	 * @param description
+	 */
+
+
+    private InputPort fastaInPort = getInputPorts().createPort("fasta");
+	private InputPort namesInPort = getInputPorts().createPort("names");
+
+    private OutputPort alignOutPort = getOutputPorts().createPort("align");
+	private OutputPort alignReportOutPort = getOutputPorts().createPort("align.report");
+	private OutputPort flipAccnosOutPort = getOutputPorts().createPort("flip.accnos");
+
+
+	public MothurAlignOperator(OperatorDescription description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
